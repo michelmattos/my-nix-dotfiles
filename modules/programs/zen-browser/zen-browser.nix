92509fd320d298893f67bd791709666dc38a66ec
Michel Mattos <7696411+michelmattos@users.noreply.github.com>
2025-12-04 21:14:10 +1300

    Added some extensions to zen browser

--- modules/programs/zen-browser/zen-browser.nix
+++ modules/programs/zen-browser/zen-browser.nix
@@ -11,5 +11,28 @@
     nativeMessagingHosts = [
       pkgs.firefoxpwa
     ];
+
+    policies = {
+      ExtensionSettings = {
+        # Proton Pass
+        "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = "allowed";
+        };
+        # Dark Reader
+        "addon@darkreader.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = "allowed";
+        };
+        # uBlock Origin
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = "allowed";
+        };
+      };
+    };
   };
 }
